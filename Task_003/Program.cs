2ffa0085b2dd73bc5ee4271a5b679131b9c7d5c3
Daniils_Kackins <daniil.kackin@gmail.com>
2023-07-15 18:56:40 +0100

    Добавил задачу номер 52

---
+++ Task_003/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+void GetColumnAverage(int[,] matrix, int n)
+{
+    Console.WriteLine("Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average += matrix[i, j];
+        }
+        average = average / n;
+        Console.Write(average + "\t");
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+int[,] getArray = GetMatrix(rows, columns, minValue, maxValue);
+PrintMatrix(getArray);
+GetColumnAverage(getArray, rows);
+
